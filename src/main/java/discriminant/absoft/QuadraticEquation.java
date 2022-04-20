fe1a891af80d72589f166ba7e41c68ea4c3ee44a
artem <avlov88@gmail.com>
2022-04-20 15:13:06 +0300

    test cases

---
+++ src/main/java/discriminant/absoft/QuadraticEquation.java
@@ -0,0 +1,31 @@
+package discriminant.absoft;
+
+import java.util.ArrayList;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+
+public class QuadraticEquation {
+    public static ArrayList solveQuadraticEquation(int a, int b, int c) {
+        double discriminant = pow(b, 2) - 4 * a * c;
+        if (discriminant >= 0 && a != 0) {
+            double x1, x2;
+            x1 = (-b + sqrt(discriminant))/(2 * a);
+            x2 = (-b - sqrt(discriminant))/(2 * a);
+            ArrayList<Double> actualSquareRoots = new ArrayList<>();
+            actualSquareRoots.add(x1);
+            actualSquareRoots.add(x2);
+            System.out.println(discriminant);
+            System.out.println("Square roots X1 and X2 = " + actualSquareRoots);
+            return actualSquareRoots;
+            }
+        else if (a == 0) {
+            System.out.println("a = 0, this equation is linear");
+            return null;
+        } else {
+            System.out.println("Discriminant les then 0 or a = 0");
+            return null;
+        }
+    }
+}
---
+++ src/test/java/discriminant/absoft/QuadraticEquationTest.java
@@ -0,0 +1,67 @@
+package discriminant.absoft;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import static java.lang.Math.*;
+
+public class QuadraticEquationTest {
+
+    /**
+     * Method verifies that quadratic equation has two different roots and these roots are calculated correctly
+     */
+    @Test
+    public void discriminantIsGreaterThanZero(){
+        //D = 49
+        int a = 2;
+        int b = 3;
+        int c = -5;
+        ArrayList squareRoots = QuadraticEquation.solveQuadraticEquation(a, b, c);
+        Assert.assertTrue(squareRoots.size() == 2 && a * pow((Double) squareRoots.get(0),2) +
+                b * (Double) squareRoots.get(0) + c == 0 && a * pow((Double) squareRoots.get(1),2) +
+                b * (Double) squareRoots.get(1) + c == 0, "Something wrong with quadratic equation roots " +
+                squareRoots);
+    }
+
+    /**
+     * Method verifies that quadratic equation has two the same roots and these roots are calculated correctly
+     */
+    @Test
+    public void discriminantIsEqualToZero(){
+        //D = 0
+        int a = 1;
+        int b = -6;
+        int c = 9;
+        ArrayList squareRoots = QuadraticEquation.solveQuadraticEquation(a, b, c);
+        Assert.assertTrue(squareRoots.size() == 2 && squareRoots.get(0) == (squareRoots.get(1))
+                && a * pow((Double) squareRoots.get(0),2) + b * (Double) squareRoots.get(0) + c == 0,
+                "Something wrong with quadratic equation roots " + squareRoots);
+    }
+
+    /**
+     * Method verifies that quadratic equation has no roots as discriminant < 0
+     */
+    @Test
+    public void discriminantIsLessThanZero(){
+        //D = -131
+        int a = 5;
+        int b = 3;
+        int c = 7;
+        Assert.assertTrue(QuadraticEquation.solveQuadraticEquation(a, b, c) == null,
+                "The discriminant of quadratic equation roots < 0, this equation has no roots");
+    }
+
+    /**
+     * Method verifies that an equation is linear as known parameter a = 0
+     */
+    @Test
+    public void knownArgumentAIsZero(){
+        //equation is linear
+        int a = 0;
+        int b = 3;
+        int c = 7;
+        Assert.assertTrue(QuadraticEquation.solveQuadraticEquation(a, b, c) == null,
+                "a = 0, this equation is linear");
+    }
+}
